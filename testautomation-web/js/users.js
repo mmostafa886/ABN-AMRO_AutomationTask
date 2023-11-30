2d3c838f0f52ece31441bda3e8b26b6116e3c6a8
mmostafa886 <mmostafa886@users.noreply.github.com>
2023-11-30 19:01:45 +0000

    Automated deployment: Thu Nov 30 19:01:45 UTC 2023 fb1092a4eb497ac7166a89a6a884994626269e27

--- testautomation-web/js/users.js
+++
@@ -1,5 +0,0 @@
-const users = [
-    {email: 'admin@admin.com', password: '2020'},
-    {email: 'biancunha@gmail.com', password: '123456'},
-    {email: 'growdev@growdev.com.br', password: 'growdev123'}
-];
